e2db33efd3e85404c4b8ba1f9868ef5f2ec1d3f5
Kevin Stoop <433243@student.fontys.nl>
2022-02-24 09:25:23 +0100

    first commit
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Navbar } from './components/Navbar'
+import logo from './logo.svg'
+import './styles/global.scss'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navbar.scss';
+
+export const Navbar = () => {
+  return (
+    <nav className='rd-navbar'>
+        <div>menu</div>
+        <div>Revolved Design</div>
+        <div>cart</div>
+    </nav>
+  )
+}
